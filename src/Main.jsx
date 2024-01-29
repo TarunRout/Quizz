f89d9d334b2b5f7cfdc3c3f73f66e158145a19aa
Tarun Rout <103878343+TarunRout@users.noreply.github.com>
2024-01-29 13:09:49 +0000

    Quiz funtionality till video 13 (29.1.2024)

--- src/Main.jsx
+++
@@ -1,5 +0,0 @@
-function Main({ children }) {
-  return <main className="main"> {children} Test</main>;
-}
-
-export default Main;
---
+++ src/components/FinishedScreen.jsx
@@ -0,0 +1,16 @@
+export default function FinishedScree({
+  points,
+  maxPossiblePoints,
+  highscore,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p>
+        You scored <strong> {points} </strong> out of {maxPossiblePoints}
+        {Math.ceil(percentage)}
+      </p>
+      <p className="highscore">( Highscore: {highscore} points )</p>
+    </>
+  );
+}
